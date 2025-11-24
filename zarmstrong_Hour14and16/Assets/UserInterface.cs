f86f2b956f0b8ca2ca013a4bcce14f28ed2db230
withSer64-a <zaneda2004@gmail.com>
2025-11-24 13:31:42 -0800

    Hour 14

---
+++ zarmstrong_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,4 @@
+/*I changed the text size, style, and color
+  I changed the button hover color to green and the select color to blue
+  I changed the game screen to have a sprite and some text.
+ */
