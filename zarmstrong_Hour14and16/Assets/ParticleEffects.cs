799aa1264143b8ef448cf48000c9d0585c7436bb
withSer64-a <zaneda2004@gmail.com>
2025-11-24 15:03:41 -0800

    Hour 16

---
+++ zarmstrong_Hour14and16/Assets/ParticleEffects.cs
@@ -0,0 +1,7 @@
+/*
+I increased the starting speed to 100
+I changed the starting color to pink
+I changed the rate over time to 100
+I added a burst with infinite cycles and 0.1 interval
+I changed the shape mode to ping-pong
+*/
